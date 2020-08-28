84d691bf8f84ad8e069cc01a01cfcb6742086606
Alexey Morozov <alexey1562@gmail.com>
2020-08-28 15:45:12 +0300

    Реализовано пятое задание второго урока.

--- lesson-2/2l_MA.playground/Contents.swift
+++ lesson-2/2l_MA.playground/Contents.swift
@@ -71,3 +71,46 @@ func removeNumbersFrom(array: [Int]) -> [Int] {
 }
 
 removeNumbersFrom(array: array)
+
+
+/* ///////////////////////////////////////////////////////////////////////
+/// Написать функцию, которая добавляет в массив новое число Фибоначчи,///
+////////////// и добавить при помощи нее 50 элементов.////////////////////
+/////////Числа Фибоначчи определяются соотношениями Fn=Fn-1 + Fn-2. //////
+/////////////////////////////////////////////////////////////////////// */
+
+func createFibonacciArray(size: Int) -> [Int] {
+    var array: [Int] = [0, 1]
+    for i in 2...size {
+        let newElement = array[i - 2] + array[i - 1]
+        array.append(newElement)
+    }
+    
+    return array
+}
+
+func addFibbonacci(to array: [Int], numberOfElements: Int) -> [Int] {
+    guard array.count > 3 else {
+        return createFibonacciArray(size: numberOfElements)
+    }
+    
+    guard numberOfElements > 0 else {
+        return array
+    }
+    
+    var result: [Int] = array
+    
+    for _ in 0..<numberOfElements {
+        let arraySize = result.count
+        let newElement = result[arraySize - 2] + result[arraySize - 1]
+        
+        result.append(newElement)
+    }
+    
+    return result
+}
+
+let fibArr = createFibonacciArray(size: 10)
+fibArr.count  // Создается на 1 больше чем указан size, потому что первый элемент "0" считается 0-ым
+
+let newFibArr = addFibbonacci(to: fibArr, numberOfElements: 50)
