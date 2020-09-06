75505d1191c90da2bc1e4ead538677ac47891bcb
Alexey Morozov <alexey1562@gmail.com>
2020-09-06 16:50:50 +0300

    Реализованы 4-6 задания четвертого урока. Добавлены исправления в удаление багажа из багажника.

--- lesson-4/4l_MA.playground/Contents.swift
+++ lesson-4/4l_MA.playground/Contents.swift
@@ -12,10 +12,10 @@ struct Baggage {
     let space: Int
 }
 
-enum Transmission {
-    case auto
-    case manual
-    case none
+enum Transmission: String {
+    case auto = "Автоматическая"
+    case manual = "Механическая"
+    case none = "Отсутствует"
 }
 
 class Car {
@@ -104,6 +104,7 @@ class Car {
             let baggage = baggageArray[i]
             if baggage.id == id {
                 _ = decreaseTrunkSpace(baggage.space)
+                baggageArray.remove(at: i)
                 result = true
                 break
             }
@@ -118,7 +119,7 @@ class Car {
             return
         }
         
-        print("Содержимое багажника:")
+        print("\nСодержимое багажника:")
         for baggage in baggageArray {
             print("\(baggage.name) занимает \(baggage.space) пространства багажника.")
         }
@@ -131,7 +132,7 @@ class Car {
             Модель: \(model)
             Год выпуска: \(year)
             Цвет: \(color)
-            Трансмиссия: \(transmission)
+            Трансмиссия: \(transmission.rawValue)
             Пробег: \(mileage)
             Мощность двигателя (лс): \(hp)
             Масса: \(weight)
@@ -166,28 +167,28 @@ class Car {
 /// Описать в каждом наследнике специфичные для него свойства. ////////////
 //////////////////////////////////////////////////////////////////////// */
 
-enum TruckType {
-    case mixer
-    case mobileCrane
-    case dumpTruck
-    case garbageTruck
-    case logCarrier
-    case truck
+enum TruckType: String {
+    case mixer = "Автомеситель"
+    case mobileCrane = "Грузоподъемный кран"
+    case dumpTruck = "Самосвал"
+    case garbageTruck = "Мусоровоз"
+    case logCarrier = "Перевозчик бревен"
+    case truck = "Тягач"
 }
 
-enum BodyType {
-    case buggy
-    case cabriolet
-    case coupe
-    case hatchback
-    case kombi
-    case limousine
-    case microvan
-    case minivan
-    case pickup
-    case roadster
-    case sedan
-    case wagon
+enum BodyType: String {
+    case buggy = "Багги"
+    case cabriolet = "Кабриолет"
+    case coupe = "Купе"
+    case hatchback = "Хэтчбек"
+    case kombi = "Комби"
+    case limousine = "Лимузин"
+    case microvan = "Микровэн"
+    case minivan = "Минивэн"
+    case pickup = "Пикап"
+    case roadster = "Родстер"
+    case sedan = "Седан"
+    case wagon = "Универсал"
 }
 
 //class TrunkCar: Car {
@@ -249,6 +250,77 @@ enum WindowStatus: String {
     case close = "Окна закрыты"
 }
 
+//class TrunkCar: Car {
+//    let truckType: TruckType
+//    let liftingCapacity: Int
+//    let cargoWeight: Int
+//    var trailerState: TrailerStatus {
+//        didSet {
+//            print("Грузовик \(model). Статус трейлера: \(trailerState.rawValue)")
+//        }
+//    }
+//
+//    init?(truckType: TruckType, liftingCapacity: Int, cargoWeight: Int, model: String, year: UInt16, trunkSpace: Int, baggageArray: [Baggage], color: UIColor, bluetooth: Bool, mileage: Int, transmission: Transmission, hp: UInt, weight: UInt, trailerState: TrailerStatus) {
+//        guard liftingCapacity > cargoWeight else {
+//            return nil
+//        }
+//
+//        self.truckType = truckType
+//        self.liftingCapacity = liftingCapacity
+//        self.cargoWeight = cargoWeight
+//        self.trailerState = trailerState
+//
+//        super.init(model: model, year: year, trunkSpace: trunkSpace, baggageArray: baggageArray, color: color, bluetooth: bluetooth, mileage: mileage, transmission: transmission, hp: hp, weight: weight)
+//    }
+//
+//    override init() {
+//        self.truckType = .truck
+//        self.liftingCapacity = 15000
+//        self.cargoWeight = 0
+//        self.trailerState = .onWay
+//        super.init()
+//    }
+//}
+//
+//let truck = TrunkCar()
+//truck.trailerState = .loading
+//truck.trailerState = .onWay
+//truck.trailerState = .unloading
+//
+//class SportCar: Car {
+//    let bodyType: BodyType
+//    let accelerationTime: Double
+//    var windowState: WindowStatus {
+//        didSet {
+//            print("Автомобиль \(model). Статус окон: \(windowState.rawValue)")
+//        }
+//    }
+//
+//    init?(model: String, year: UInt16, trunkSpace: Int, baggageArray: [Baggage], color: UIColor, bluetooth: Bool, mileage: Int, transmission: Transmission, hp: UInt, weight: UInt, bodyType: BodyType, accelerationTime: Double, windowState: WindowStatus) {
+//        self.bodyType = bodyType
+//        self.accelerationTime = accelerationTime
+//        self.windowState = windowState
+//        super.init(model: model, year: year, trunkSpace: trunkSpace, baggageArray: baggageArray, color: color, bluetooth: bluetooth, mileage: mileage, transmission: transmission, hp: hp, weight: weight)
+//    }
+//
+//    override init() {
+//        self.bodyType = .sedan
+//        self.accelerationTime = 10.0
+//        self.windowState = .close
+//        super.init()
+//    }
+//}
+
+//let sportCar = SportCar()
+//sportCar.windowState = .close
+//sportCar.windowState = .open
+
+
+/* /////////////////////////////////////////////////////////
+/// 4. В каждом подклассе переопределить метод /////////////
+/// действия с автомобилем в соответствии с его классом. ///
+///////////////////////////////////////////////////////// */
+
 class TrunkCar: Car {
     let truckType: TruckType
     let liftingCapacity: Int
@@ -279,12 +351,28 @@ class TrunkCar: Car {
         self.trailerState = .onWay
         super.init()
     }
+    
+    override func printStatus() {
+        print("""
+            \n
+            Тип: Грузовой
+            Модель: \(model)
+            Год выпуска: \(year)
+            Цвет: \(color)
+            Трансмиссия: \(transmission.rawValue)
+            Пробег: \(mileage)
+            Мощность двигателя (лс): \(hp)
+            Масса: \(weight)
+            Тип: \(truckType.rawValue)
+            Грузоподъемность \(liftingCapacity)
+            Вес груза: \(cargoWeight)
+            Состояние трейлера: \(trailerState.rawValue)
+            Объем багажника: \(trunkSpace)
+            Использованный объем багажника: \(usedTrunkSpace)
+            \n
+            """)
+    }
 }
-
-//let truck = TrunkCar()
-//truck.trailerState = .loading
-//truck.trailerState = .onWay
-//truck.trailerState = .unloading
 
 class SportCar: Car {
     let bodyType: BodyType
@@ -308,28 +396,92 @@ class SportCar: Car {
         self.windowState = .close
         super.init()
     }
+    
+    override func printStatus() {
+        print("""
+            \n
+            Тип: Легковой
+            Модель: \(model)
+            Год выпуска: \(year)
+            Цвет: \(color)
+            Трансмиссия: \(transmission.rawValue)
+            Пробег: \(mileage)
+            Мощность двигателя (лс): \(hp)
+            Масса: \(weight)
+            Тип: \(bodyType.rawValue)
+            Разгон (0 - 100км/ч): \(accelerationTime)
+            Состояние окон: \(windowState.rawValue)
+            Объем багажника: \(trunkSpace)
+            Использованный объем багажника: \(usedTrunkSpace)
+            \n
+            """)
+    }
 }
-
-//let sportCar = SportCar()
-//sportCar.windowState = .close
-//sportCar.windowState = .open
-
-
-/* /////////////////////////////////////////////////////////
-/// 4. В каждом подклассе переопределить метод /////////////
-/// действия с автомобилем в соответствии с его классом. ///
-///////////////////////////////////////////////////////// */
-
-
 
 /* //////////////////////////////////////////////////
 /// 5. Создать несколько объектов каждого класса. ///
 /// Применить к ним различные действия. /////////////
 ////////////////////////////////////////////////// */
 
+// Багаж(личные вещи)
+let pc = Baggage(id: 0, name: "PC", description: "Обычный PC", space: 30)
+let chair = Baggage(id: 1, name: "Стул", description: "Обычный стул", space: 70)
+let table = Baggage(id: 2, name: "Стол", description: "Обычный стол", space: 180)
+let wardrobe = Baggage(id: 3, name: "Шкаф", description: "Огромный шкаф", space: 500)
+
+//let trunkCar = TrunkCar()
+let trunkCar = TrunkCar(truckType: .truck, liftingCapacity: 25000, cargoWeight: 10000, model: "MAN", year: 2018, trunkSpace: 1500, baggageArray: [], color: .red, bluetooth: true, mileage: 1500, transmission: .auto, hp: 700, weight: 37000, trailerState: .onWay)
+
+if let trunkCar = trunkCar {
+    trunkCar.printStatus()
+    trunkCar.printBaggage()
+
+    trunkCar.addBaggage(pc)
+    trunkCar.addBaggage(chair)
+    trunkCar.addBaggage(table)
+    trunkCar.addBaggage(wardrobe)
+    trunkCar.printBaggage()
+
+    trunkCar.removeBaggageBy(table.id)
+    trunkCar.removeBaggageBy(chair.id)
+    trunkCar.printBaggage()
+}
+
+//let sportCar = SportCar()
+let sportCar = SportCar(model: "Toyota", year: 1998, trunkSpace: 300, baggageArray: [], color: .orange, bluetooth: false, mileage: 330000, transmission: .manual, hp: 280, weight: 1270, bodyType: .coupe, accelerationTime: 5.8, windowState: .close)
+
+if let car = sportCar {
+    car.printStatus()
+    car.printBaggage()
+
+    car.addBaggage(pc)
+    car.addBaggage(chair)
+    car.addBaggage(table)
+    car.printBaggage()
+
+    if (!car.addBaggage(wardrobe)) {
+        print("\n\(wardrobe.name) не поместился в \(car.model)\n")
+    }
+    
+    car.printBaggage()
+
+    car.removeBaggageBy(table.id)
+    car.removeBaggageBy(chair.id)
+    car.printBaggage()
+}
 
 
 /* ///////////////////////////////////////////////////////
 /// 6. Вывести значения свойств экземпляров в консоль. ///
 /////////////////////////////////////////////////////// */
 
+if let trunkCar = trunkCar {
+    trunkCar.trailerState = .loading
+    trunkCar.trailerState = .onWay
+    trunkCar.trailerState = .unloading
+}
+
+if let car = sportCar {
+    car.windowState = .close
+    car.windowState = .open
+}
