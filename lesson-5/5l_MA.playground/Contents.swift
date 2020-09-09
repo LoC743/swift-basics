313a8f4e3385cd8ddd17250232626fa108027df4
Alexey Morozov <alexey1562@gmail.com>
2020-09-09 14:08:31 +0300

    Реализовано второе задание пятого урока.

--- lesson-5/5l_MA.playground/Contents.swift
+++ lesson-5/5l_MA.playground/Contents.swift
@@ -7,10 +7,20 @@ struct Baggage {
     let space: Int
 }
 
-enum Transmission {
-    case auto
-    case manual
-    case none
+enum Transmission: String {
+    case auto = "Автоматическая"
+    case manual = "Механическая"
+    case none = "Отсутствует"
+}
+
+enum EngineStatus: String {
+    case running = "Запущен"
+    case stopped = "Не запущен"
+}
+
+enum WindowsStauts: String {
+    case open = "Открыты"
+    case close = "Закрыты"
 }
 
 /* ///////////////////////////////////////////////////
@@ -22,13 +32,16 @@ protocol Car {
     var model: String { get }
     var year: UInt16 { get }
     var trunkSpace: Int { get }
-    var usedTrunkSpace: Int { get }
+    var usedTrunkSpace: Int { get set }
     var color: UIColor { get }
     var bluetooth: Bool { get }
     var mileage: Int { get }
-    var transmission: Transmission { get }
     var hp: UInt { get }
     var weight: UInt { get }
+    var transmission: Transmission { get }
+    
+    var engineState: EngineStatus { get set }
+    var windowsState: WindowsStauts { get set }
     
     var baggageArray: [Baggage] { get set }
     
@@ -37,6 +50,12 @@ protocol Car {
     
     func addBaggage(_ baggage: Baggage) -> Bool
     func removeBaggageBy(_ id: Int) -> Bool
+    
+    func openWindows()
+    func closeWindows()
+    
+    func startEngine()
+    func stopEngine()
     
     func printBaggage()
     func printStatus()
@@ -52,6 +71,109 @@ protocol Car {
 ///////////////////////////////////////////////////////////////////////////// */
 
 
+// Управление двигателем
+extension Car {
+    mutating func startEngine() {
+        engineState = .running
+    }
+    
+    mutating func stopEngine() {
+        engineState = .stopped
+    }
+}
+
+// Управление окнами
+extension Car {
+    mutating func openWindows() {
+        windowsState = .open
+    }
+    
+    mutating func closeWindows() {
+        windowsState = .close
+    }
+}
+
+// Управление багажником
+extension Car {
+    private mutating func increaseTrunkSpace(_ space: Int) -> Bool {
+        var result = false
+        if usedTrunkSpace + space <= trunkSpace {
+            usedTrunkSpace += space
+            result = true
+        }
+        
+        return result
+    }
+    
+    private mutating func decreaseTrunkSpace(_ space: Int) -> Bool {
+        var result = false
+        if usedTrunkSpace - space >= 0 {
+            usedTrunkSpace -= space
+            result = true
+        }
+        
+        return result
+    }
+    
+    mutating func addBaggage(_ baggage: Baggage) -> Bool {
+        var result = false
+        if increaseTrunkSpace(baggage.space) {
+            baggageArray.append(baggage)
+            result = true
+        }
+        
+        return result
+    }
+    
+    mutating func removeBaggageBy(_ id: Int) -> Bool {
+        var result = false
+        for i in 0..<baggageArray.count {
+            let baggage = baggageArray[i]
+            if baggage.id == id {
+                _ = decreaseTrunkSpace(baggage.space)
+                baggageArray.remove(at: i)
+                result = true
+                break
+            }
+        }
+        
+        return result
+    }
+}
+
+// Вывод информации о полях Car
+extension Car {
+    func printBaggage() {
+        guard baggageArray.count > 0 else {
+            print("Багажник пуст.")
+            return
+        }
+        
+        print("\nСодержимое багажника:")
+        for baggage in baggageArray {
+            print("\(baggage.name) занимает \(baggage.space) пространства багажника.")
+        }
+        print("Занято \(usedTrunkSpace) из \(trunkSpace)\n")
+    }
+    
+    func printStatus() {
+        print("""
+            \n
+            Модель: \(model)
+            Год выпуска: \(year)
+            Цвет: \(color)
+            Трансмиссия: \(transmission.rawValue)
+            Пробег: \(mileage)
+            Мощность двигателя (лс): \(hp)
+            Масса: \(weight)
+            Состояние окон: \(windowsState.rawValue)
+            Состояние двигателя: \(engineState.rawValue)
+            Объем багажника: \(trunkSpace)
+            Использованный объем багажника: \(usedTrunkSpace)
+            \n
+            """)
+    }
+}
 
 /* //////////////////////////////////////////////////////////////////////////////////
 /// 3. Создать два класса, имплементирующих протокол «Car» - trunkCar и sportСar. ///
