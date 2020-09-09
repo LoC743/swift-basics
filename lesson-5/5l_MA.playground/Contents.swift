19b6388b10487a7f8676f4e767c933cc21d2a2b7
Alexey Morozov <alexey1562@gmail.com>
2020-09-09 14:42:20 +0300

    Реализовано третье задание пятого урока.

--- lesson-5/5l_MA.playground/Contents.swift
+++ lesson-5/5l_MA.playground/Contents.swift
@@ -23,6 +23,36 @@ enum WindowsStauts: String {
     case close = "Закрыты"
 }
 
+enum TrailerStatus: String {
+    case unloading = "Производится разгрузка"
+    case loading = "Производится загрузка"
+    case onWay = "В пути"
+}
+
+enum TruckType: String {
+    case mixer = "Автомеситель"
+    case mobileCrane = "Грузоподъемный кран"
+    case dumpTruck = "Самосвал"
+    case garbageTruck = "Мусоровоз"
+    case logCarrier = "Перевозчик бревен"
+    case truck = "Тягач"
+}
+
+enum BodyType: String {
+    case buggy = "Багги"
+    case cabriolet = "Кабриолет"
+    case coupe = "Купе"
+    case hatchback = "Хэтчбек"
+    case kombi = "Комби"
+    case limousine = "Лимузин"
+    case microvan = "Микровэн"
+    case minivan = "Минивэн"
+    case pickup = "Пикап"
+    case roadster = "Родстер"
+    case sedan = "Седан"
+    case wagon = "Универсал"
+}
+
 /* ///////////////////////////////////////////////////
 /// 1. Создать протокол «Car» и описать свойства,  ///
 /// общие для автомобилей, а также метод действия. ///
@@ -45,20 +75,20 @@ protocol Car {
     
     var baggageArray: [Baggage] { get set }
     
-    func increaseTrunkSpace(_ space: Int) -> Bool
-    func decreaseTrunkSpace(_ space: Int) -> Bool
+//    func increaseTrunkSpace(_ space: Int) -> Bool
+//    func decreaseTrunkSpace(_ space: Int) -> Bool
     
-    func addBaggage(_ baggage: Baggage) -> Bool
-    func removeBaggageBy(_ id: Int) -> Bool
-    
-    func openWindows()
-    func closeWindows()
-    
-    func startEngine()
-    func stopEngine()
-    
-    func printBaggage()
-    func printStatus()
+//    func addBaggage(_ baggage: Baggage) -> Bool
+//    func removeBaggageBy(_ id: Int) -> Bool
+//
+//    func openWindows()
+//    func closeWindows()
+//
+//    func startEngine()
+//    func stopEngine()
+//
+//    func printBaggage()
+//    func printStatus()
 }
 
 
@@ -180,7 +210,150 @@ extension Car {
 /// Описать в них свойства, отличающиеся для спортивного автомобиля и цистерны.   ///
 ////////////////////////////////////////////////////////////////////////////////// */
 
+class TrunkCar: Car {
+    var model: String
+    var year: UInt16
+    var trunkSpace: Int
+    var usedTrunkSpace: Int
+    var color: UIColor
+    var bluetooth: Bool
+    var mileage: Int
+    var hp: UInt
+    var weight: UInt
+    var transmission: Transmission
+    
+    var engineState: EngineStatus = .stopped
+    var windowsState: WindowsStauts = .close
+    
+    var baggageArray: [Baggage]
+    
+    let liftingCapacity: Int
+    let cargoWeight: Int
+    let truckType: TruckType
+    let trailerState: TrailerStatus
+    
+    
+    init?(truckType: TruckType, liftingCapacity: Int, cargoWeight: Int, model: String, year: UInt16, trunkSpace: Int, baggageArray: [Baggage], color: UIColor, bluetooth: Bool, mileage: Int, transmission: Transmission, hp: UInt, weight: UInt, trailerState: TrailerStatus) {
+        guard liftingCapacity > cargoWeight else {
+            return nil
+        }
+        
+        self.usedTrunkSpace = 0
+        for baggage in baggageArray {
+            self.usedTrunkSpace += baggage.space
+        }
+        
+        guard usedTrunkSpace <= trunkSpace else {
+            return nil
+        }
+        
+        self.model = model
+        self.year = year
+        self.trunkSpace = trunkSpace
+        self.baggageArray = baggageArray
+        self.color = color
+        self.bluetooth = bluetooth
+        self.mileage = mileage
+        self.transmission = transmission
+        self.hp = hp
+        self.weight = weight
 
+        self.truckType = truckType
+        self.liftingCapacity = liftingCapacity
+        self.cargoWeight = cargoWeight
+        self.trailerState = trailerState
+    }
+    
+    func printStatus() {
+        print("""
+            \n
+            Тип: Грузовой
+            Модель: \(model)
+            Год выпуска: \(year)
+            Цвет: \(color)
+            Трансмиссия: \(transmission.rawValue)
+            Пробег: \(mileage)
+            Мощность двигателя (лс): \(hp)
+            Масса: \(weight)
+            Тип: \(truckType.rawValue)
+            Грузоподъемность \(liftingCapacity)
+            Вес груза: \(cargoWeight)
+            Состояние окон: \(windowsState.rawValue)
+            Состояние двигателя: \(engineState.rawValue)
+            Состояние трейлера: \(trailerState.rawValue)
+            Объем багажника: \(trunkSpace)
+            Использованный объем багажника: \(usedTrunkSpace)
+            \n
+            """)
+    }
+}
+
+class SportCar: Car {
+    var model: String
+    var year: UInt16
+    var trunkSpace: Int
+    var usedTrunkSpace: Int
+    var color: UIColor
+    var bluetooth: Bool
+    var mileage: Int
+    var hp: UInt
+    var weight: UInt
+    var transmission: Transmission
+    
+    var engineState: EngineStatus = .stopped
+    var windowsState: WindowsStauts = .close
+    
+    var baggageArray: [Baggage]
+    
+    let bodyType: BodyType
+    let accelerationTime: Double
+    
+    init?(model: String, year: UInt16, trunkSpace: Int, baggageArray: [Baggage], color: UIColor, bluetooth: Bool, mileage: Int, transmission: Transmission, hp: UInt, weight: UInt, bodyType: BodyType, accelerationTime: Double) {
+        self.usedTrunkSpace = 0
+        for baggage in baggageArray {
+            self.usedTrunkSpace += baggage.space
+        }
+        
+        guard usedTrunkSpace <= trunkSpace else {
+            return nil
+        }
+        
+        self.bodyType = bodyType
+        self.accelerationTime = accelerationTime
+        
+        self.model = model
+        self.year = year
+        self.trunkSpace = trunkSpace
+        self.baggageArray = baggageArray
+        self.color = color
+        self.bluetooth = bluetooth
+        self.mileage = mileage
+        self.transmission = transmission
+        self.hp = hp
+        self.weight = weight
+    }
+    
+    func printStatus() {
+        print("""
+            \n
+            Тип: Легковой
+            Модель: \(model)
+            Год выпуска: \(year)
+            Цвет: \(color)
+            Трансмиссия: \(transmission.rawValue)
+            Пробег: \(mileage)
+            Мощность двигателя (лс): \(hp)
+            Масса: \(weight)
+            Тип: \(bodyType.rawValue)
+            Разгон (0 - 100км/ч): \(accelerationTime)
+            Состояние окон: \(windowsState.rawValue)
+            Состояние двигателя: \(engineState.rawValue)
+            Объем багажника: \(trunkSpace)
+            Использованный объем багажника: \(usedTrunkSpace)
+            \n
+            """)
+    }
+}
 
 /* ///////////////////////////////////////////////////////
 /// 4. Для каждого класса написать расширение,         ///
