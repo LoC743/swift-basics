832209a7bcadc845b60aa7345a1cbbb79f537a6e
Alexey Morozov <alexey1562@gmail.com>
2020-09-12 17:58:29 +0300

    Реализовано первое задание шестого урока.

--- lesson-6/6l_MA.playground/Contents.swift
+++ lesson-6/6l_MA.playground/Contents.swift
@@ -2,6 +2,66 @@
 /// 1. Реализовать свой тип коллекции «очередь» (queue) c использованием дженериков. ///
 ///////////////////////////////////////////////////////////////////////////////////// */
 
+struct Queue<T> {
+    private var elements: [T]
+    private var maxSize: UInt
+    
+    init(maxSize: UInt) {
+        elements = []
+        
+        self.maxSize = maxSize
+    }
+    
+    mutating func add(_ item: T) -> Bool {
+        var result = false
+        
+        if elements.count < maxSize {
+            elements.append(item)
+            result = true
+        }
+        
+        return result
+    }
+    
+    mutating func remove() -> T? {
+        var element: T? = nil
+        
+        if elements.count > 0 {
+            element = elements.removeFirst()
+        }
+        
+        return element
+    }
+    
+    func peek() -> T? {
+        return elements.first
+    }
+    
+    func isFull() -> Bool {
+        return elements.count == maxSize
+    }
+    
+    func isEmpty() -> Bool {
+        return elements.count == 0
+    }
+}
+
+var queue = Queue<Int>(maxSize: 2)
+queue.add(1)
+queue.add(2)
+queue.add(3)
+queue.isFull()
+queue.isEmpty()
+
+queue.remove()
+queue.remove()
+queue.remove()
+queue.isFull()
+queue.isEmpty()
+
+queue.add(100)
+queue.isFull()
+queue.isEmpty()
 
 
 /* //////////////////////////////////////////////////////////////
